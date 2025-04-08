2e0831929ea13fc7c1d811dc1092f60f4500acde
Bernard2806 <berramuspegonzalez@gmail.com>
2025-04-08 07:56:47 -0300

    Añadir clases Prestamo y Usuario con propiedades
    Se han añadido propiedades y comentarios en la clase `Prestamo` en el archivo `Prestamo.cs`, incluyendo identificadores únicos para el préstamo, libro y usuario, así como fechas de préstamo y devolución.
    
    En el archivo `Usuario.cs`, se ha agregado la clase `Usuario` con propiedades para el identificador, nombres, apellidos y una lista de préstamos, junto con comentarios descriptivos para cada propiedad. También se han añadido las directivas `using` necesarias para el funcionamiento del código.

--- BibliotecaEFCore/Prestamo.cs
+++ BibliotecaEFCore/Prestamo.cs
@@ -8,5 +8,42 @@ namespace BibliotecaEFCore
 {
     public class Prestamo
     {
+        /// <summary>
+        /// Identificador único del préstamo.
+        /// </summary>
+        public int Id { get; set; }
+
+
+        /// <summary>
+        /// Identificador único del libro prestado.
+        /// </summary>
+        public int LibroId { get; set; }
+
+        /// <summary>
+        /// Libro prestado.
+        /// </summary>
+        public Libro Libro { get; set; }
+
+
+        /// <summary>
+        /// Identificador único del usuario que realiza el préstamo.
+        /// </summary>
+        public int UsuarioId { get; set; }
+
+        /// <summary>
+        /// Usuario que realiza el préstamo.
+        /// </summary>
+        public Usuario Usuario { get; set; }
+
+
+        /// <summary>
+        /// Fecha en la que se realizó el préstamo.
+        /// </summary>
+        public DateTime FechaPrestamo { get; set; }
+
+        /// <summary>
+        /// Fecha en la que se debe devolver el libro.
+        /// </summary>
+        public DateTime? FechaDevolucion { get; set; }
     }
 }
---
+++ BibliotecaEFCore/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaEFCore
+{
+    public class Usuario
+    {
+        /// <summary>
+        /// Identificador único del usuario.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombres del usuario.
+        /// </summary>
+        public string Nombres { get; set; } = null!;
+
+        /// <summary>
+        /// Apellidos del usuario.
+        /// </summary>
+        public string Apellidos { get; set; } = null!;
+
+        /// <summary>
+        /// Lista de préstamos realizados por el usuario.
+        /// </summary>
+        public List<Prestamo> Prestamos { get; set; } = new();
+    }
+}
