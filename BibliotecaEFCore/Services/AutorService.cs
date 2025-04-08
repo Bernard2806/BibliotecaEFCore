fdce284d0046523b09b393a69fd3395ed5a1f2b2
Bernard2806 <berramuspegonzalez@gmail.com>
2025-04-08 08:13:47 -0300

    Añadir servicio para la gestión de autores
    Se ha implementado la interfaz `IAutorService` y la clase `AutorService` en `AutorService.cs`.
    La nueva funcionalidad permite crear autores en el contexto de la base de datos `BibliotecaContext`,
    almacenando los datos y mostrando un mensaje de confirmación en la consola.

---
+++ BibliotecaEFCore/Services/AutorService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaEFCore.Data.Models;
+using BibliotecaEFCore.Data;
+
+namespace BibliotecaEFCore.Services
+{
+    public interface IAutorService
+    {
+        Autor CrearAutor(string nombres, string apellidos);
+    }
+
+    public class AutorService : IAutorService
+    {
+        private readonly BibliotecaContext _context;
+        public AutorService(BibliotecaContext context) => _context = context;
+
+        public Autor CrearAutor(string nombres, string apellidos)
+        {
+            var autor = new Autor { Nombres = nombres, Apellidos = apellidos };
+            _context.Autores.Add(autor);
+            _context.SaveChanges();
+            Console.WriteLine($"Autor creado: {nombres}, {apellidos}");
+            return autor;
+        }
+    }
+}
