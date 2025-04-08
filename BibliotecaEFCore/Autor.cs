0f6644f882833f323bd121bdb6d37f6b519155c7
Bernard2806 <berramuspegonzalez@gmail.com>
2025-04-08 07:52:26 -0300

    Agregar archivos de proyecto.

---
+++ BibliotecaEFCore/Autor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaEFCore
+{
+    /// <summary>
+    /// Clase que representa un autor de libros.
+    /// </summary>
+    public class Autor
+    {
+        /// <summary>
+        /// Identificador único del autor.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombres del autor.
+        /// </summary>
+        public string Nombres { get; set; } = null!;
+
+        /// <summary>
+        /// Apellidos del autor.
+        /// </summary>
+        public string Apellidos { get; set; } = null!;
+
+        /// <summary>
+        /// Lista de libros publicados por el autor.
+        /// </summary>
+        public List<Libro> LibrosPublicados { get; set; } = new();
+    }
+}
---
+++ BibliotecaEFCore/Libro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaEFCore
+{
+    /// <summary>
+    /// Clase que representa un libro en la biblioteca.
+    /// </summary>
+    public class Libro
+    {
+        /// <summary>
+        /// Identificador único del libro.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Título del libro.
+        /// </summary>
+        public string Titulo { get; set; } = null!;
+
+        /// <summary>
+        /// Año de publicación del libro.
+        /// </summary>
+        public int AnioPublicacion { get; set; }
+
+
+        /// <summary>
+        /// Identificador único del autor del libro.
+        /// </summary>
+        public int AutorId { get; set; }
+
+        /// <summary>
+        /// Autor del libro.
+        /// </summary>
+        [ForeignKey("AutorId")]
+        public Autor Autor { get; set; } = null!;
+
+        /// <summary>
+        /// Lista de préstamos asociados al libro.
+        /// </summary>
+        public List<Prestamo> Prestamos { get; set; } = new();
+    }
+}
